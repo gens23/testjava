b9a1532a5e8258b208dd97535e2f6308a8e8ff95
gens23 <43158357+gens23@users.noreply.github.com>
2021-10-13 16:41:35 -0500

    Se agregó nueva variable entera para guardar número.

--- src/JavaDev/holaMundo.java
+++ src/JavaDev/holaMundo.java
@@ -2,6 +2,10 @@ package JavaDev;
 
 public class holaMundo {
 	public static void main(String[] args) {
-		System.out.println( "Hola mundo jueee" );
+		System.out.println("Hola Mundo");
+		
+		int a = 1;
+		
+		System.out.println( "\n a es igual a " + a );
 	}
 }
